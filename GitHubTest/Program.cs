62c36696e96d670ab5a30f2eddec244862972669
Derek <derekwisher5@gmail.com>
2025-08-22 06:22:17 -0700

    Now its a method!

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Academy!! One more modification!");
+﻿MyMethod();
+void MyMethod()
+{
+    Console.WriteLine("Now I have a method!!");
+}
